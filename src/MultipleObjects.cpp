2b3ca207392a17d01d2d866de258925cdd31317e
Jean Catanho <jensmcatanho@gmail.com>
2016-02-05 04:56:24 -0200

    Update the class MultipleObjects to ShadeRecord's new name.

--- src/MultipleObjects.cpp
+++ src/MultipleObjects.cpp
@@ -12,7 +12,7 @@ MultipleObjects::~MultipleObjects()
 {}
 
 RGBColor MultipleObjects::trace_ray(const Ray& ray) const {
-	ShadeRec sr(worldPtr->hit_objects(ray));
+	ShadeRecord sr(worldPtr->hit_objects(ray));
 
 	if (sr.hit)
 		return sr.color;
