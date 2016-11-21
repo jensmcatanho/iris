a70606968e93a946fb7c5bf1ff8d1ad49d125d5b
Jean Catanho <jensmcatanho@gmail.com>
2016-11-21 17:25:06 -0200

    Implement the BRDF base class

---
+++ Source/BRDFs/include/BRDF.h
@@ -0,0 +1,50 @@
+/*
+-----------------------------------------------------------------------------
+The MIT License (MIT)
+
+Copyright (c) 2016 Jean Michel Catanho
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+-----------------------------------------------------------------------------
+*/
+#ifndef BRDF_H
+#define BRDF_H
+
+#include "Prerequisites.h"
+
+class BRDF {
+	public:
+		BRDF();
+		BRDF(std::shared_ptr<Sampler>);
+
+		virtual RGBColor f(const ShadeRecord &, const glm::vec3 &, const glm::vec3 &) const;
+
+		virtual RGBColor SampleF(ShadeRecord &, const glm::vec3 &, glm::vec3 &);
+
+		virtual RGBColor SampleF(ShadeRecord &, const glm::vec3 &, glm::vec3 &, float &);
+
+		virtual RGBColor rho(const ShadeRecord &, const glm::vec3 &) const;
+
+		void SetSampler(std::shared_ptr<Sampler>);
+
+	protected:
+		std::shared_ptr<Sampler> m_SamplerPtr;
+};
+
+#endif
