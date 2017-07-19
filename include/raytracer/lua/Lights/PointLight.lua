1af96ef3fdef3a4f595182b7be8ebc4a73c6988e
Jean Catanho <jensmcatanho@gmail.com>
2017-07-18 22:39:37 -0300

    Fix typing error in Lua library

--- include/raytracer/lua/Lights/PointLight.lua
+++ include/raytracer/lua/Lights/PointLight.lua
@@ -8,7 +8,7 @@ function PointLight.new()
 	-- Public member variables
 	self.name = "Point"
 	self.position = {x = 0.0, y = 0.0, z = 0.0}
-	self.color = {r = 1.0, g = 1.0, b = .0}
+	self.color = {r = 1.0, g = 1.0, b = 1.0}
 	self.intensity = 1.0
 
 	-- Public methods
