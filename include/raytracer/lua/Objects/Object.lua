585de1ae41a56d88be4c083b2cd1f29963a15c74
Jean Catanho <jensmcatanho@gmail.com>
2017-08-03 02:57:22 -0300

    Correct license comments for lua files

--- include/raytracer/lua/Objects/Object.lua
+++ include/raytracer/lua/Objects/Object.lua
@@ -1,4 +1,4 @@
-[[-----------------------------------------------------------------------------
+--[[
 The MIT License (MIT)
 
 Copyright (c) 2016-2017 Jean Michel Catanho
@@ -20,7 +20,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
------------------------------------------------------------------------------]]
+--]]
 Object = {}
 
 function Object.new()
--- include/raytracer/lua/config.lua
+++ include/raytracer/lua/config.lua
@@ -1,4 +1,4 @@
-[[-----------------------------------------------------------------------------
+--[[
 The MIT License (MIT)
 
 Copyright (c) 2016-2017 Jean Michel Catanho
@@ -20,6 +20,6 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
------------------------------------------------------------------------------]]
+--]]
 package.path = "include/raytracer/lua/?.lua;" .. package.path
 local Scene = require('Scene')
